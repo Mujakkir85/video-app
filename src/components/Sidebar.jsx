c36033d18ca8f8b4fdee6fa5c78300d0e81f377e
Mujakkir85 <mujakkirlucse@gmail.com>
2023-04-17 01:49:22 +0600

    added categories design and videos page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,9 @@ import { Stack } from '@mui/material';
 import React from 'react';
 import { categories } from '../utils/constants';
 
+
+const selectedCategory = 'New'
+
 const Sidebar = () => (
     <Stack
     direction="row"
@@ -11,10 +14,17 @@ const Sidebar = () => (
         flexDirection: {md: 'column'}
     }}
     >
-    {categories.map((categorie)=>(
-        <button>
-            <span>{categorie.icon}</span>
-            <span>{categorie.name}</span>
+    {categories.map((category)=>(
+        <button
+        className="category-btn"
+        style={{
+            background: category.name === selectedCategory && '#FC1503',
+            color: 'white'
+        }}
+        key={category.name}
+        >
+            <span style={{ color: category.name === selectedCategory ?  "white" : "red", marginRight: "15px" }}>{category.icon}</span>
+            <span style={{ opacity: category.name === selectedCategory ? "1": "0.8" }} >{category.name}</span>
         </button>
     ))}
        
---
+++ src/components/Videos.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Videos = () =>  (
+        <div>
+            Videos
+        </div>
+    );
+
+
+export default Videos;
